687bd3052f4136ac35f00329213ab595e2674c9f
Paolo Ligsay <paolomartinligsay@gmail.com>
2020-10-29 22:59:25 +0800

    added fetch

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,16 @@ let user_id = 'matt'
 let client_id = 'd61d37e0b87d4d8cabb9a61358a5c912'
 let scope = 'playlist-read-private'
 
-curl -H "Authorization: Basic ZDYxZDM3ZTBiODdkNGQ4Y2FiYjlhNjEzNThhNWM5MTI6YjUxOGRjNGFjYjIyNDE1MDk2ZjY0Yzk4Y2QyNzA1MDI=" -d grant_type=authorization_code -d code=MQCbtKe...44KN -d redirect_uri=https%3A%2F%2Fpaolomartinligsay.netlify.app%2F https://accounts.spotify.com/api/token
 
-https://accounts.spotify.com/authorize?client_id=d61d37e0b87d4d8cabb9a61358a5c912&response_type=code&redirect_uri=https%3A%2F%2Fpaolomartinligsay.netlify.app%2F&scopes=playlist-read-private
+fetch(
+  `https://api.spotify.com/v1/playlists/${7I6Xs1Nx6avnEMelOxUFfC}/tracks`
+).then(
+  res => res.json()
+).then(
+  data => console.log(data)
+)
+
+
+
+// curl -H "Authorization: Basic ZDYxZDM3ZTBiODdkNGQ4Y2FiYjlhNjEzNThhNWM5MTI6YjUxOGRjNGFjYjIyNDE1MDk2ZjY0Yzk4Y2QyNzA1MDI=" -d grant_type=authorization_code -d code=MQCbtKe...44KN -d redirect_uri=https%3A%2F%2Fpaolomartinligsay.netlify.app%2F https://accounts.spotify.com/api/token
+// https://accounts.spotify.com/authorize?client_id=d61d37e0b87d4d8cabb9a61358a5c912&response_type=code&redirect_uri=https%3A%2F%2Fyourspotifyprofile.netlify.app%2F&scopes=playlist-read-private
