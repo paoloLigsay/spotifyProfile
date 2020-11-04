b45b46dd4bc17765e8660d7a23fe340c42bc8352
Paolo Ligsay <paolomartinligsay@gmail.com>
2020-11-04 13:02:14 +0800

    param

--- src/main.js
+++ src/main.js
@@ -3,51 +3,24 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 // components
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Discover from './components/discover'
-=======
-import Users from './components/user'
->>>>>>> 1462769... add login
-=======
 import Discover from './components/discover'
->>>>>>> ecb8aad... get profile
 import Profile from './components/profile'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const routes = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8900588... commit three message
-=======
->>>>>>> 8f705e6... print data sample
   { 
     path: '/discover/:display_name',
     component: Discover,
-    props: route => ({ query: route.query.q })
+    props: true
   },
   { 
     path: '/',
-    component: Profile
-  },
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  { path: '/users', component: Users },
-=======
-  { path: '/discover', component: Discover },
->>>>>>> ecb8aad... get profile
-  { path: '/', component: Profile },
->>>>>>> 1462769... add login
-=======
->>>>>>> 8900588... commit three message
-=======
->>>>>>> 8f705e6... print data sample
+    component: Profile,
+    props: true
+  }
 ]
 
 const router = new VueRouter({
