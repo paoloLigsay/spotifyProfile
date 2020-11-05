cf4e56722cff52759f7a13c4448bfe59b75ab6ee
Paolo Ligsay <paolomartinligsay@gmail.com>
2020-11-05 21:27:22 +0800

    remove offset

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -136,7 +136,7 @@
           )
       },
       get_user_playlists (local_access_token) {
-        return fetch("https://api.spotify.com/v1/me/playlists?offset=2", {
+        return fetch("https://api.spotify.com/v1/me/playlists", {
           headers: {
             Authorization: `Bearer ${local_access_token}`
           }
