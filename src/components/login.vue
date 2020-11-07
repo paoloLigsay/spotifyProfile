66a7614c301d1fb2c30489ab26b2bf966bd288b4
Paolo Ligsay <paolomartinligsay@gmail.com>
2020-11-08 00:07:31 +0800

    main

--- src/components/login.vue
+++ src/components/login.vue
@@ -4,6 +4,7 @@
       <a class="btn btn--login" href="https://accounts.spotify.com/authorize?client_id=d61d37e0b87d4d8cabb9a61358a5c912&response_type=code&redirect_uri=https%3A%2F%2Fyourspotifyprofile.netlify.app%2F&scope=playlist-read-private user-read-private user-read-email user-read-recently-played user-top-read user-follow-read">
         LOGIN
       </a>
+      <p class="text text--12 footer"> Designed and Developed by <span class="text--green"> Paolo Ligsay </span> </p>
     </div>
 </template>
 
