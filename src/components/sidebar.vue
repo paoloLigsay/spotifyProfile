5eaf8110e6445a2ca06683877ecf79e3332c4d9b
Paolo Ligsay <paolomartinligsay@gmail.com>
2020-11-08 14:44:41 +0800

    main

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -55,7 +55,7 @@
         sidebar_items[item_number].classList.add('sidebar__item--active')
       }
     },
-    mounted() {
+    created() {
       const active_number = this.active
       this.set_active(active_number)
     }
