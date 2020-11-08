d4132355ad5cddc8e3d7160ad092f9149f41c0cd
Paolo Ligsay <paolomartinligsay@gmail.com>
2020-11-08 22:31:43 +0800

    main

--- src/components/artists.vue
+++ src/components/artists.vue
@@ -59,7 +59,7 @@
               const new_artist = {
                 name: artist.name,
                 followers: format_followers,
-                images: artist.images[0],
+                images: artist.images,
                 url: artist.external_urls.spotify
               }
 
