6f1496fa167e340e6d12af0bc85aed17c81f2645
Paolo Ligsay <paolomartinligsay@gmail.com>
2020-11-08 15:47:54 +0800

    main

--- src/components/playlist.vue
+++ src/components/playlist.vue
@@ -9,11 +9,11 @@
     <!-- component -->
     <div class="playlist">
       <h2> PUBLIC PLAYLIST </h2>
-       <div class="discover__playlist">
-        <a :href="d_playlist_item.url" class="discover__playlist-item" v-for="(d_playlist_item, i) in d_playlist" :key="i">
-          <img :src="d_playlist_item.image" alt="playlist image" class="discover__playlist-img">
-          <p class="text text--21"> {{ d_playlist_item.name }} by {{ d_playlist_item.owner }} </p>
-          <p class="text"> {{ d_playlist_item.track_count }} Tracks </p>
+       <div class="playlist__list">
+        <a :href="d_playlist_item.url" class="playlist__list-item" v-for="(d_playlist_item, i) in d_playlist" :key="i">
+          <img :src="d_playlist_item.image" alt="playlist image" class="playlist__list-img">
+          <p class="text text--36"> {{ d_playlist_item.name }} by {{ d_playlist_item.owner }} </p>
+          <p class="text text--24"> {{ d_playlist_item.track_count }} Tracks </p>
         </a>
       </div>
     </div>
@@ -42,6 +42,7 @@
         })
           .then(res => res.json())
           .then(data => {
+            console.log(data.items)
             const playlists = data.items
             for(let playlist of playlists) {
               const new_playlist = {
